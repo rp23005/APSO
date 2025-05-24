31d9a0c49d85d7392024b62d5ac1631dbad138be
retro2047 <134110393+retro2047@users.noreply.github.com>
2025-05-24 12:01:36 -0600

    Mi parte de la Conexion de NEON
    Esto es mi parte de la conexion de NEON

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,6 +18,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
+       if (System.getenv("SPRING_PROFILES_ACTIVE") == null) {
+        Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
         SpringApplication.run(Application.class, args);
     }
 }
